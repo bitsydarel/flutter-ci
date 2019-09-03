e441df6a5044319fb10f747753863c280a2fc7d6
bitsydarel <bitsydarel@gmail.com>
2019-09-03 19:30:44 +0300

    update dockerfile to use script

--- scripts/setup_for_emulator.sh
+++ scripts/setup_for_emulator.sh
@@ -3,7 +3,8 @@
 if [ "$WITH_EMULATOR" = "false" ];
 then
   echo "running without emulator";
-  wget -O /usr/bin/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x /usr/bin/android-wait-for-emulator;
+  flutter emulators
 else
+  wget -O /usr/bin/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x /usr/bin/android-wait-for-emulator;
   flutter emulators --create --name android_emulator -v;
 fi
