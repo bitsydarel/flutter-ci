0c576e0d33768482deb0d206164978bf6a24f74d
bitsydarel <bitsydarel@gmail.com>
2019-09-04 16:14:50 +0300

    enabled build linux docker with emulator by default.

--- scripts/setup_for_emulator.sh
+++ scripts/setup_for_emulator.sh
@@ -5,6 +5,7 @@ then
   echo "running without emulator";
   flutter emulators
 else
+  yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;x86"
   wget -O /usr/bin/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x /usr/bin/android-wait-for-emulator;
   flutter emulators --create --name android_emulator -v;
 fi
