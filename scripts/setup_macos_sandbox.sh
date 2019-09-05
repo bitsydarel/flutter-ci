8a908239d01deb796bfb3dc49be89f7233fb18fd
bitsydarel <bitsydarel@gmail.com>
2019-09-05 17:01:17 +0300

    test configuration necessary to boot up sandbox

--- scripts/setup_macos_sandbox.sh
+++ scripts/setup_macos_sandbox.sh
@@ -53,61 +53,78 @@ VBoxManage modifyvm $VIRTUAL_MACHINE_NAME --cpus 2
 
 VBoxManage modifyvm $VIRTUAL_MACHINE_NAME --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
 
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemFamily" "MacBook Pro"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacBookPro11,2"
+VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac11,3"
 
 VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
 
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemUuid" "CAFECAFE-CAFE-CAFE-CAFE-DECAFFDECAFF"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemVendor" "Apple Inc."
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemSerial" "C02LJ6QSFD56"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiOEMVBoxVer" "string:1"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiOEMVBoxRev" "string:.23456"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiBIOSVersion" "string:MBP7.89"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Mac-3CBD00234E554E41"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiBoardSerial" "C02LJ6QSFDW1A"
+VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
 
 VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0000/Uuid" "4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0000/Name" "MLB"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0000/Value" "bytes:$(echo -n "C02LJ6QSFDW1A" | base64)"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0001/Uuid" "4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0001/Name" "ROM"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0001/Value" "%aa*%bbg%cc%dd"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0002/Uuid" "7C436110-AB2A-4BBB-A880-FE41995C9F82"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0002/Name" "system-id"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0002/Value" "aabbccddeeff00112233445566778899"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0003/Uuid" "7C436110-AB2A-4BBB-A880-FE41995C9F82"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0003/Name" "csr-active-config"
-
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0003/Value" "0x10"
-
 VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 0
 
-VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal2/EfiGopMode" 4
+VBoxManage setextradata $VIRTUAL_MACHINE_NAME VBoxInternal2/EfiGopMode 4
 
 VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal2/EfiGraphicsResolution" "1280x800"
+
+
+#VBoxManage modifyvm $VIRTUAL_MACHINE_NAME --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
+
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemFamily" "MacBook Pro"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacBookPro11,2"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemUuid" "CAFECAFE-CAFE-CAFE-CAFE-DECAFFDECAFF"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemVendor" "Apple Inc."
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiSystemSerial" "C02LJ6QSFD56"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiOEMVBoxVer" "string:1"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiOEMVBoxRev" "string:.23456"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiBIOSVersion" "string:MBP7.89"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Mac-3CBD00234E554E41"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/Config/DmiBoardSerial" "C02LJ6QSFDW1A"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0000/Uuid" "4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0000/Name" "MLB"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0000/Value" "bytes:$(echo -n "C02LJ6QSFDW1A" | base64)"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0001/Uuid" "4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0001/Name" "ROM"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0001/Value" "%aa*%bbg%cc%dd"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0002/Uuid" "7C436110-AB2A-4BBB-A880-FE41995C9F82"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0002/Name" "system-id"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0002/Value" "aabbccddeeff00112233445566778899"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0003/Uuid" "7C436110-AB2A-4BBB-A880-FE41995C9F82"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0003/Name" "csr-active-config"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/efi/0/LUN#0/Config/Vars/0003/Value" "0x10"
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 0
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal2/EfiGopMode" 4
+#
+#VBoxManage setextradata $VIRTUAL_MACHINE_NAME "VBoxInternal2/EfiGraphicsResolution" "1280x800"
 
 VBoxManage modifyvm $VIRTUAL_MACHINE_NAME --vram 256
 
