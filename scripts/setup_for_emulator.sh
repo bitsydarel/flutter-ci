124f8d79253c6e7c43c1cdb6b5db9d9f3c638a2e
bitsydarel <bitsydarel@gmail.com>
2019-09-28 02:07:50 +0300

    added possibility to run docker with unknown user.

--- scripts/setup_for_emulator.sh
+++ scripts/setup_for_emulator.sh
@@ -8,6 +8,6 @@ then
   flutter emulators
 else
   yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;x86"
-  wget -O /usr/bin/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x /usr/bin/android-wait-for-emulator;
+  wget -O "$TOOLS_HOME"/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x "$TOOLS_HOME"/android-wait-for-emulator;
   flutter emulators --create --name android_emulator -v;
 fi
