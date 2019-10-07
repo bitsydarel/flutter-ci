789ef13adc487c5a90442bba9372a987dd80a56a
bitsydarel <bitsydarel@gmail.com>
2019-10-07 16:49:20 +0300

    added argument for android emulator architecture.

--- scripts/setup_for_emulator.sh
+++ scripts/setup_for_emulator.sh
@@ -7,8 +7,8 @@ then
   echo "running without emulator";
   avdmanager list avd
 else
-  yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;x86"
+  yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;$ANDROID_EMULATOR_ARCH"
   wget -O "$TOOLS_HOME"/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x "$TOOLS_HOME"/android-wait-for-emulator;
   wget -O "$TOOLS_HOME"/launch_android_emulator.sh https://raw.githubusercontent.com/bitsydarel/flutter-ci/master/scripts/launch_android_emulator.sh && chmod +x "$TOOLS_HOME"/launch_android_emulator.sh
-  avdmanager create avd -n android_emulator -k "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;x86"
+  avdmanager create avd -n android_emulator -k "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;$ANDROID_EMULATOR_ARCH"
 fi
