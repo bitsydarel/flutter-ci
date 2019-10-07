4dd54ddeecd3b500d6e03425a2281bbe348f673d
bitsydarel <bitsydarel@gmail.com>
2019-10-07 18:05:48 +0300

    add input no to avdMnager create command.

--- scripts/setup_for_emulator.sh
+++ scripts/setup_for_emulator.sh
@@ -10,5 +10,5 @@ else
   yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;$ANDROID_EMULATOR_ARCH"
   wget -O "$TOOLS_HOME"/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x "$TOOLS_HOME"/android-wait-for-emulator;
   wget -O "$TOOLS_HOME"/launch_android_emulator.sh https://raw.githubusercontent.com/bitsydarel/flutter-ci/master/scripts/launch_android_emulator.sh && chmod +x "$TOOLS_HOME"/launch_android_emulator.sh
-  no | avdmanager create avd -n android_emulator -k "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;$ANDROID_EMULATOR_ARCH"
+  yes no | avdmanager create avd -n android_emulator -k "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;$ANDROID_EMULATOR_ARCH"
 fi
