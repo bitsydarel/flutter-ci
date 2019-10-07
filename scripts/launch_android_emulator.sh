8670d76d31972645bc10582a203e0e1a3c4de9f1
bitsydarel <bitsydarel@gmail.com>
2019-10-07 16:35:15 +0300

    Divided docker image

---
+++ scripts/launch_android_emulator.sh
@@ -0,0 +1,1 @@
+cd "$TOOLS_HOME" && emulator -avd android_emulator -noaudio -no-boot-anim -gpu off && ./android-wait-for-emulator
--- scripts/setup_for_emulator.sh
+++ scripts/setup_for_emulator.sh
@@ -5,9 +5,10 @@
 if [ "$WITH_EMULATOR" = "false" ];
 then
   echo "running without emulator";
-  flutter emulators
+  avdmanager list avd
 else
-  yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;x86"
+  yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;x86_64"
   wget -O "$TOOLS_HOME"/android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator && chmod +x "$TOOLS_HOME"/android-wait-for-emulator;
-  flutter emulators --create --name android_emulator -v;
+  wget -O "$TOOLS_HOME"/launch_android_emulator.sh https://raw.githubusercontent.com/bitsydarel/flutter-ci/master/scripts/launch_android_emulator.sh && chmod +x "$TOOLS_HOME"/launch_android_emulator.sh
+  avdmanager create avd -n android_emulator -k "system-images;android-$ANDROID_PLATFORM_VERSION;google_apis_playstore;x86"
 fi
