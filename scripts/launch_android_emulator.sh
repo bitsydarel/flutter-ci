6cb8a84b23677ae12387a8889d4c73e3ae0b280e
bitsydarel <bitsydarel@gmail.com>
2019-10-08 17:02:36 +0300

    tweaked launch emulator script to write to file

--- scripts/launch_android_emulator.sh
+++ scripts/launch_android_emulator.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cd "$TOOLS_HOME" && emulator -avd android_emulator -no-window -noaudio -no-boot-anim -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -verbose -debug all &
+adb start-server
+{ emulator -avd android_emulator -no-window -noaudio -no-boot-anim -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -show-kernel -verbose -debug all > emulator.log; } 2>&1 &
 android-wait-for-emulator
