1e860aae1e669ec2c763776e1e2e91f9067d363a
bitsydarel <bitsydarel@gmail.com>
2019-10-08 20:40:44 +0300

    added set e+ to exit on script error

--- scripts/launch_android_emulator.sh
+++ scripts/launch_android_emulator.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 ## maintainer Darel Bitsy <bitsydarel@gmail.com>
+
+set +e
 
 adb start-server
 { emulator -avd android_emulator -no-window -noaudio -no-boot-anim -delay-adb -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -show-kernel -verbose -debug all > emulator.log; } 2>&1 &
