65e2ec49a5bfc099f1c29ea3d33c45d9db6d745d
bitsydarel <bitsydarel@gmail.com>
2019-10-08 18:42:41 +0300

    fixed typo in wait for android emulator script

--- scripts/setup_for_emulator.sh
+++ scripts/setup_for_emulator.sh
@@ -8,7 +8,7 @@ then
   avdmanager list avd
 else
   yes | sdkmanager "system-images;android-$ANDROID_PLATFORM_VERSION;$ANDROID_EMULATOR_TYPE;$ANDROID_EMULATOR_ARCH"
-  wget -O "$TOOLS_HOME"/android-wait-for-emulator https://raw.githubusercontent.com/bitsydarel/flutter-ci/master/scripts/wait_for_android_emulator.sh && chmod +x "$TOOLS_HOME"/wait_for_android_emulator.sh;
+  wget -O "$TOOLS_HOME"/wait_for_android_emulator.sh https://raw.githubusercontent.com/bitsydarel/flutter-ci/master/scripts/wait_for_android_emulator.sh && chmod +x "$TOOLS_HOME"/wait_for_android_emulator.sh;
   wget -O "$TOOLS_HOME"/launch_android_emulator.sh https://raw.githubusercontent.com/bitsydarel/flutter-ci/master/scripts/launch_android_emulator.sh && chmod +x "$TOOLS_HOME"/launch_android_emulator.sh
   printf 'no\n' | avdmanager create avd -n android_emulator -k "system-images;android-$ANDROID_PLATFORM_VERSION;$ANDROID_EMULATOR_TYPE;$ANDROID_EMULATOR_ARCH"
 fi
--- scripts/wait_for_android_emulator.sh
+++ scripts/wait_for_android_emulator.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 ## maintainer Darel Bitsy <bitsydarel@gmail.com>
-set +e
 
 emulator_booted=""
 failcounter=0
 timeout_in_sec=900
 
 until [[ "$emulator_booted" =~ "1" ]]; do
-  emulator_booted=`adb -e shell getprop sys.boot_completed 2>&1 &`
-  if [[ "$emulator_booted" =~ "device not found" || "$emulator_booted" =~ "device offline"
-    || "$emulator_booted" =~ "running" ]]; then
-    let "failcounter += 1"
+  emulator_booted=$(adb -e shell getprop sys.boot_completed 2>&1 &)
+  if [[ "$emulator_booted" =~ "device not found" || "$emulator_booted" =~ "device offline" || "$emulator_booted" =~ "running" ]]; then
+    ((failcounter += 1))
     echo "Waiting for emulator to start"
     if [[ $failcounter -gt timeout_in_sec ]]; then
       echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
