352829b6fb8095f98a1fe93d84e47d88f4ec9b97
bitsydarel <bitsydarel@gmail.com>
2019-10-08 18:14:13 +0300

    updated the wait emulator script

---
+++ scripts/wait_for_android_emulator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## maintainer Darel Bitsy <bitsydarel@gmail.com>
+
+set +e
+
+emulator_booted=""
+failcounter=0
+timeout_in_sec=900
+
+until [[ "$emulator_booted" =~ "1" ]]; do
+  emulator_booted=$(adb -e shell getprop sys.boot_completed 2>&1 &)
+  if [[ "$emulator_booted" =~ "device not found" || "$emulator_booted" =~ "device offline" || "$emulator_booted" =~ "running" ]]; then
+    ((failcounter += 1))
+    echo "Waiting for emulator to start"
+    if [[ $failcounter -gt timeout_in_sec ]]; then
+      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+      exit 1
+    fi
+  fi
+  sleep 1
+done
+
+echo "Emulator is ready"
