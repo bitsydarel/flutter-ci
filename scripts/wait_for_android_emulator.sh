50e75423eb5790b078d9db323ea64f4b67e3b236
bitsydarel <bitsydarel@gmail.com>
2019-10-08 20:22:21 +0300

    changed script for timeout

--- scripts/wait_for_android_emulator.sh
+++ scripts/wait_for_android_emulator.sh
@@ -4,19 +4,19 @@
 
 emulator_booted=""
 failcounter=0
-timeout_in_sec=900
+retry_counter=15
 
 until [[ "$emulator_booted" =~ "1" ]]; do
   emulator_booted=$(adb -e shell getprop sys.boot_completed 2>&1 &)
   if [[ "$emulator_booted" =~ "device not found" || "$emulator_booted" =~ "device offline" || "$emulator_booted" =~ "running" ]]; then
     ((failcounter += 1))
     echo "Waiting for emulator to start"
-    if [[ $failcounter -gt timeout_in_sec ]]; then
-      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+    if [[ $failcounter -gt retry_counter ]]; then
+      echo "Retry count $retry_counter reached; failed to start emulator"
       exit 1
     fi
   fi
-  sleep 1
+  sleep 60
 done
 
 echo "Emulator is ready"
