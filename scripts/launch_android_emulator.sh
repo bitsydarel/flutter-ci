ba631b06c2a0966ae33615917b457081e3e4fc9c
bitsydarel <bitsydarel@gmail.com>
2019-10-08 04:58:33 +0300

    added hook to make parameterized build

--- scripts/launch_android_emulator.sh
+++ scripts/launch_android_emulator.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cd "$TOOLS_HOME" && emulator -avd android_emulator -no-window -noaudio -no-boot-anim -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -verbose -debug all && ./android-wait-for-emulator
+cd "$TOOLS_HOME" && emulator -avd android_emulator -no-window -noaudio -no-boot-anim -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -verbose -debug all &
+./android-wait-for-emulator
