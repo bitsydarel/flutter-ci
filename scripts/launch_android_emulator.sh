2abcd7d00a33698c2458aadd12880630f785ec77
bitsydarel <bitsydarel@gmail.com>
2019-10-08 18:17:23 +0300

    updated the wait emulator script

--- scripts/launch_android_emulator.sh
+++ scripts/launch_android_emulator.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+## maintainer Darel Bitsy <bitsydarel@gmail.com>
+
 adb start-server
 { emulator -avd android_emulator -no-window -noaudio -no-boot-anim -delay-adb -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -show-kernel -verbose -debug all > emulator.log; } 2>&1 &
 wait_for_android_emulator.sh
