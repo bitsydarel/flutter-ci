4d5a846b6b3fdfadfbc363f5d5ef784529c2e776
bitsydarel <bitsydarel@gmail.com>
2019-10-08 03:50:57 +0300

    Added option to disable hardware acceleration and also disable window.

--- scripts/launch_android_emulator.sh
+++ scripts/launch_android_emulator.sh
@@ -1,1 +1,2 @@
-cd "$TOOLS_HOME" && emulator -avd android_emulator -noaudio -no-boot-anim -gpu off && ./android-wait-for-emulator
+#!/bin/sh
+cd "$TOOLS_HOME" && emulator -avd android_emulator -no-window -noaudio -no-boot-anim -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -verbose -debug all && ./android-wait-for-emulator
