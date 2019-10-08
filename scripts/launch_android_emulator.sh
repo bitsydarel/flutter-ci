0f2f7e048bec0b31e66d6ea4bb0fe72d93c1357f
bitsydarel <bitsydarel@gmail.com>
2019-10-08 17:15:32 +0300

    added -delay-adb parameter to emulator that allow wait for full emulator boot up before handling adb command.

--- scripts/launch_android_emulator.sh
+++ scripts/launch_android_emulator.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 adb start-server
-{ emulator -avd android_emulator -no-window -noaudio -no-boot-anim -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -show-kernel -verbose -debug all > emulator.log; } 2>&1 &
+{ emulator -avd android_emulator -no-window -noaudio -no-boot-anim -delay-adb -gpu swiftshader_indirect -accel "$ANDROID_EMULATOR_ACCEL" -show-kernel -verbose -debug all > emulator.log; } 2>&1 &
 android-wait-for-emulator
