c25075343b06071a31ade2ecc347af2980f5335d
bitsydarel <bitsydarel@gmail.com>
2019-12-23 14:32:24 +0200

    added destination folder from gradle installation script

--- scripts/setup_gradle.sh
+++ scripts/setup_gradle.sh
@@ -5,6 +5,6 @@
 if [ "$WITH_GRADLE" = "false" ]; then
   echo "Skipping gradle installation"
 else
-  wget -O "$TOOLS_HOME"/gradle.zip https\://services.gradle.org/distributions/gradle-"$GRADLE_VERSION"-all.zip && unzip "$TOOLS_HOME"/gradle.zip
+  wget -O "$TOOLS_HOME"/gradle.zip https\://services.gradle.org/distributions/gradle-"$GRADLE_VERSION"-all.zip && unzip "$TOOLS_HOME"/gradle.zip -d "$TOOLS_HOME"
   rm -rf "$TOOLS_HOME"/gradle.zip
 fi
