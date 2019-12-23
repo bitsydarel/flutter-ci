c928dbade3b7daca7e774894059f9b870fe2e4f0
bitsydarel <bitsydarel@gmail.com>
2019-12-23 13:23:43 +0200

    removed export to env from gradle installation script

--- scripts/setup_gradle.sh
+++ scripts/setup_gradle.sh
@@ -6,5 +6,5 @@ if [ "$WITH_GRADLE" = "false" ]; then
   echo "Skipping gradle installation"
 else
   wget -O "$TOOLS_HOME"/gradle.zip https\://services.gradle.org/distributions/gradle-"$GRADLE_VERSION"-all.zip && unzip "$TOOLS_HOME"/gradle.zip
-  rm -rf "$TOOLS_HOME"/gradle.zip && export PATH="$PATH":"$TOOLS_HOME"/gradle-"$GRADLE_VERSION":"$TOOLS_HOME"/gradle-"$GRADLE_VERSION"/bin
+  rm -rf "$TOOLS_HOME"/gradle.zip
 fi
