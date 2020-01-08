e2dba124f3f801dfd4de94f494894d2741fd1668
bitsydarel <bitsydarel@gmail.com>
2020-01-08 13:57:45 +0200

    Added VNC setup script

---
+++ scripts/setup_vnc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## maintainer Darel Bitsy <bitsydarel@gmail.com>
+
+set +e
+
+mkdir -p "${HOME}"/.vnc
+
+x11vnc -storepasswd "${VNC_PASSWORD}" "${HOME}"/.vnc/passwd
+
+ls -a "${HOME}" && ls -a "${HOME}"/.vnc
+
+x11vnc -forever -usepw -create &
