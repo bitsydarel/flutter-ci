fdbc3b3f0dd1a13880e8dc3089a1f357814961f1
bitsydarel <bitsydarel@gmail.com>
2020-02-03 00:20:22 +0200

    removed unused env variables

--- scripts/install_flutter.sh
+++ scripts/install_flutter.sh
@@ -3,21 +3,22 @@
 ## maintainer Darel Bitsy <bitsydarel@gmail.com>
 
 if [ -z "$1" ]; then flutter_version="stable"; else flutter_version="$1"; fi
+if [ -z "$VAGRANT_HOME" ]; then homeDir="$HOME"; else homeDir="$VAGRANT_HOME"; fi
 
 ENV_FILE=/etc/profile
 
 sudo touch $ENV_FILE
 
-sudo echo export TOOLS_HOME="$TOOLS_HOME" | sudo tee -a $ENV_FILE >/dev/null
-sudo echo export FLUTTER_HOME="$TOOLS_HOME/flutter" | sudo tee -a $ENV_FILE >/dev/null
-sudo echo export FLUTTER_ROOT="$TOOLS_HOME/flutter" | sudo tee -a $ENV_FILE >/dev/null
+sudo echo export TOOLS_HOME="$homeDir/tools" | sudo tee -a $ENV_FILE >/dev/null
+sudo echo export FLUTTER_HOME="$homeDir/tools/flutter" | sudo tee -a $ENV_FILE >/dev/null
+sudo echo export FLUTTER_ROOT="$homeDir/tools/flutter" | sudo tee -a $ENV_FILE >/dev/null
 sudo echo export FLUTTER_CHANNEL="$flutter_version" | sudo tee -a $ENV_FILE >/dev/null
 
 source "$ENV_FILE"
 
 mkdir -p "$TOOLS_HOME" && cd "$TOOLS_HOME" && git clone -b "$FLUTTER_CHANNEL" https://github.com/flutter/flutter.git
 
-echo export PATH "$PATH":"$FLUTTER_HOME"/bin:"$FLUTTER_HOME"/bin/cache/dart-sdk/bin:"$HOME"/.pub-cache/bin | sudo tee -a $ENV_FILE >/dev/null
+echo export PATH "$PATH":"$FLUTTER_HOME"/bin:"$FLUTTER_HOME"/bin/cache/dart-sdk/bin:"$homeDir"/.pub-cache/bin | sudo tee -a $ENV_FILE >/dev/null
 
 source "$ENV_FILE"
 
